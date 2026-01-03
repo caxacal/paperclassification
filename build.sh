0d926e8f62ff6381b8fb6822d7b6deb4cdffe8c2
caxacal <caxacal@gmail.com>
2026-01-03 15:00:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ echo "Build starting..."
 echo "Python version:"
 python --version
 
+echo "Upgrading pip..."
+python -m pip install --upgrade pip
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
