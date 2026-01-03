e4d3f6dda483ee44815d8a65300e5429bf93edc2
caxacal <caxacal@gmail.com>
2026-01-03 21:46:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
 
-echo "Build starting..."
-echo "Python version:"
-python --version
-
-echo "Upgrading pip..."
-python -m pip install --upgrade pip
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Build completed!"
