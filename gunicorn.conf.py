9a9df30c1bb8e791625a1a1de90d77dfd7eabe99
caxacal <caxacal@gmail.com>
2026-01-05 14:54:38 +0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,20 +1,4 @@
-# gunicorn.conf.py
-
+bind = "0.0.0.0:10000"
 workers = 1
-worker_class = "sync"
-
+threads = 1
 timeout = 180
-graceful_timeout = 180
-
-preload_app = False   # 🔴 IMPORTANT: disable preload
-
-max_requests = 100
-max_requests_jitter = 20
-
-def post_fork(server, worker):
-    from app import cached_prediction
-    try:
-        cached_prediction("Warm up model for inference")
-        server.log.info("Warm-up completed in worker")
-    except Exception as e:
-        server.log.warning("Warm-up failed", exc_info=e)
