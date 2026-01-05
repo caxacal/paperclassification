a64c2243b2ddc61c88dda38d4bc61912025c9039
caxacal <caxacal@gmail.com>
2026-01-05 11:21:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -164,5 +164,5 @@ def classify():
 
 # ---------- Run ----------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    print("Use Gunicorn to run this app")
+
